d8b195421d4b19b5465df6213a4a08f49a9686e4
Bohdan Honcharuk <dariydragon26@gmail.com>
2020-03-14 13:28:47 +0200

    beautify

--- ench.kt
+++ ench.kt
@@ -1,21 +1,23 @@
 package rpgbackbone.Ench
 
-interface Template: rpgbackbone.RPGBBObject {
+interface CommandTemplate: rpgbackbone.RPGBBObject {
   val code: Code
   val change: Int
   var executable: Boolean
   fun execute()
 }
 
-abstract class Damage(val type: Code, val healthShrink: Int): Template {
-  protected var healthBar: rpgbackbone.Attribute.Changeable?= null
+abstract class Damage(val _type: Code, val healthShrink: Int): CommandTemplate {
+  override val code: Code= _type
+  override val change: Int= healthShrink
   override var executable: Boolean= false
+  protected var healthBar: rpgbackbone.Attribute.Changeable?= null
   override fun execute() {
-    if (executable) healthBar!!.value-= healthShrink
+    if (executable) healthBar!!.value-= change
   }
 }
 
-abstract class DamageWithResistanceCheck(val _type: Code, val _healthShrink: Int) : Damage(_type, _healthShrink) {
+abstract class DamageWithResistanceCheck(val damageType: Code, val _healthShrink: Int) : Damage(damageType, _healthShrink) {
   protected var resistance: Int= 0
 
   fun prepare(_toDamage: rpgbackbone.Attribute.Changeable, _resistance: rpgbackbone.Attribute.Changeable) {
@@ -25,7 +27,7 @@ abstract class DamageWithResistanceCheck(val _type: Code, val _healthShrink: Int
   }
   override fun execute() {
     if (executable) {
-      healthBar!!.value-= healthShrink/resistance
+      healthBar!!.value-= change/resistance
     }
   }
 }
