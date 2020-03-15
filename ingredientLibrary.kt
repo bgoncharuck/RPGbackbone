73786f00d860a88132bc49fd8b37c20d0b4b4ffb
Bohdan Honcharuk <dariydragon26@gmail.com>
2020-03-15 13:50:04 +0200

    ench testing

--- ingredientLibrary.kt
+++ ingredientLibrary.kt
@@ -1,29 +1,29 @@
 package rpgbackbone.Ingredient
 
-class BeeSting: Template {
-  val name: String= "Bee Sting"
-  val properties: listOf(rpgbackbone.Ench.PoisonDamage(29))
-  val isStrong: Boolean= true
-  val isWide: Boolean= false
+class BeeSting(): Template {
+  override val name: String= "Bee Sting"
+  override val properties: List<rpgbackbone.Ench.CommandTemplate> = listOf(rpgbackbone.Ench.PoisonDamage(29))
+  override val isStrong: Boolean= true
+  override val isWide: Boolean= false
 }
 
-class BuzzingGrass: Template {
-  val name: String= "Buzzing Grass"
-  val properties: listOf(rpgbackbone.Ench.PoisonDamage(2))
-  val isStrong: Boolean= false
-  val isWide: Boolean= false
+class BuzzingGrass(): Template {
+  override val name: String= "Buzzing Grass"
+  override val properties: List<rpgbackbone.Ench.CommandTemplate> = listOf(rpgbackbone.Ench.PoisonDamage(2))
+  override val isStrong: Boolean= false
+  override val isWide: Boolean= false
 }
 
-class TrueLoversKnot: Template {
-  val name: String= "True Lover's Knot"
-  val properties: listOf(rpgbackbone.Ench.PoisonDamage(33))
-  val isStrong: Boolean= true
-  val isWide: Boolean= false
+class TrueLoversKnot(): Template {
+  override val name: String= "True Lover's Knot"
+  override val properties: List<rpgbackbone.Ench.CommandTemplate> = listOf(rpgbackbone.Ench.PoisonDamage(33))
+  override val isStrong: Boolean= true
+  override val isWide: Boolean= false
 }
 
-class Cowbane: Template {
-  val name: String= "Cowbane"
-  val properties: listOf(rpgbackbone.Ench.PoisonDamage(45))
-  val isStrong: Boolean= true
-  val isWide: Boolean= false
+class Cowbane(): Template {
+  override val name: String= "Cowbane"
+  override val properties: List<rpgbackbone.Ench.CommandTemplate> = listOf(rpgbackbone.Ench.PoisonDamage(45))
+  override val isStrong: Boolean= true
+  override val isWide: Boolean= false
 }
--- main.kt
+++ main.kt
@@ -1,4 +1,6 @@
 package rpgbackbone
 
 fun main() {
+  val berry= rpgbackbone.Ingredient.TrueLoversKnot()
+  println(berry.properties.get(0).change)
 }
